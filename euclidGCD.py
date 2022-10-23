e9eb5ed02417cea78195438c462270aebcdd611f
munish0838 <munishkumar.btcse20@pec.edu.in>
2022-10-23 18:56:20 +0530

    Create euclidGCD.py

---
+++ euclidGCD.py
@@ -0,0 +1,16 @@
+# Python code to demonstrate naive
+# method to compute gcd ( recursion )
+
+
+def hcf(a, b):
+	if(b == 0):
+		return a
+	else:
+		return hcf(b, a % b)
+
+a = 60
+b = 48
+
+# prints 12
+print("The gcd of 60 and 48 is : ", end="")
+print(hcf(60, 48))
